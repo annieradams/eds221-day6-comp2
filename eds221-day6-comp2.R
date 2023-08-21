cd494f08f4ca25a500e7febdb814c67ec415dcd6
annieradams <adams.annie11@gmail.com>
2023-08-21 15:02:38 -0700

    finished examples

--- eds221-day6-comp2.R
+++ eds221-day6-comp2.R
@@ -56,5 +56,56 @@ exclude_bb_cc_pie <- pie_crab %>% filter(!site %in% c("BB","CC","PIE"))
 #create a subset from pie crab that only contains obs from sites NIB CC and ZI, for crabs with carapace size exceeding 13
 
 pie_subset<- pie_crab %>% filter(site%in% c("NIB","CC","ZI"), size>13)
+# select certain columns
+crabs_subset <- pie_crab %>% select(latitude, size, water_temp)
+#select a range of columns using a single colon
+crabs_subset2 <- pie_crab %>% select(site:air_temp)
+
+# select a range and an individual column
+crab_subset3 <- pie_crab %>% select(date:water_temp)
+
+#can re order things with select
+pie_crab %>% select(name, water_temp, size)
 
 
+------# Mutate-----
+# explicitly use dplyr mutate to add or update a column while keeping all existing columns
+
+crab_cm <- pie_crab %>% mutate(size_cm = size /10)
+
+## what happens if i use mutate to add a new column containing the mean of of the size column
+
+crab_mean_size <- pie_crab %>% mutate(mean_size = mean(size, na.rm= TRUE))
+
+
+## this is a warning it writes over the previous column called "name"
+crabs_awesome <- pie_crab %>% mutate(name = "Teddy is awesome")
+
+
+# group by site and then find mean crab aize
+mean_size_by_site <- pie_crab %>% group_by(site) %>% 
+  summarize(mean_size = mean(size, na.rm = TRUE),
+            sd_size = sd(size, na.rm = TRUE))
+# group by then mutate
+group_mutate <- pie_crab %>% 
+  group_by(site) %>% 
+  mutate(mean_size = mean(size, na.rm = TRUE))
+
+
+# create a new variable but what it contains is conditional based on previous data
+# create new column in pie chart that contains "giant" if the size is greater than 35, or "not giant" if the size is less than or equal to20
+#use dplyr::case_when() to write if else statememts more easily
+
+crabs_bin <- pie_crab %>% 
+  mutate(sie_bin = case_when (
+    size>20 ~"Giant",
+    size<=20 ~"Not giant"
+  ))
+
+
+sites_binned <- pie_crab %>%  
+  mutate(region = case_when(
+    site %in% c("ZI","CC","PIE")~ "Low",
+    site %in% c("BB","NIB")~ " Middle",
+    TRUE ~ "High"
+  ))
